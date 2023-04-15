b1a6831dd2df02c6af13363ab5fa14d6721e4348
madhuri-dhas <madhuridhas123@gmail.com>
2023-04-15 21:49:25 +0530

    Add project files.

---
+++ MVCBasic/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasic.Controllers
+{
+    //[RoutePrefix("abc")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // [Route("home")]
+        [HttpGet]
+        public ActionResult Welcome(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login( string employeename)
+        {
+            ViewBag.Name = employeename;
+            return View("~/Views/Home/Welcome.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
